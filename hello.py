a0b8a1d0e1deb5c8e1c918bf1701e6097ef8e6cb
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-02-13 15:08:57 +0900

    init

---
+++ hello.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+client = OpenAI()
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role":"system","content":"You are a helpful assistant."},
+        {"role":"user","content":"Hello! I am your personal assistant!"}
+    ],
+)
+
+print(response.to_json(indent=2))
